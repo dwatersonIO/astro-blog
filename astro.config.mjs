d718f860e776cec0aac7b7dd599067786d81ca54
dwatersonIO <dwaterson68@gmail.com>
2023-04-09 16:52:30 +0200

    added tkinter blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'; // 
-// https://astro.build/config
 
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-
 import image from "@astrojs/image";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
