da67e344a140d9f77e7544c752147333358e7c23
dwatersonIO <dwaterson68@gmail.com>
2024-03-27 06:39:38 +0100

    added blog about Django forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: ['py', 'js','sh', 'html'],
+      langs: ['py', 'js','sh', 'html', 'css'],
       // Enable word wrap to prevent horizontal scrolling
       wrap: true
     }
