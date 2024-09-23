02e6be70be8874871a38be7ad4edadea220e45c1
dwatersonIO <dwaterson68@gmail.com>
2024-09-23 19:45:35 +0200

    added a blog post on Django searches

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: ['py', 'js', 'sh', 'html', 'css'],
+      langs: ['py', 'js', 'sh', 'html', 'css', 'json'],
       // Enable word wrap to prevent horizontal scrolling
       wrap: true
     }
